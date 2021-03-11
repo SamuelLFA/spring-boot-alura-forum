4ef02a251a1039380fdcaddd4eac9c916f215448
Samuel Lima <samuellfa3@gmail.com>
2021-03-10 22:16:09 -0300

    change to jwt authentication

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -25,10 +26,12 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "topics").permitAll()
-                .antMatchers(HttpMethod.GET, "topics/*").permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin();
+            .antMatchers(HttpMethod.GET, "/topics").permitAll()
+            .antMatchers(HttpMethod.GET, "/topics/*").permitAll()
+            .antMatchers(HttpMethod.POST, "/auth").permitAll()
+            .anyRequest().authenticated()
+            .and().csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
